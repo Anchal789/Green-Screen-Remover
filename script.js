8b5ef9ad5c119b8344c80139b91a29268637d51e
Anchal Deshmukh <anchaldeshmukh789@gmail.com>
2024-05-24 23:47:38 +0530

    Final Commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const srtop = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 1000,
+    reset: true
+});
+
+// // keyfeature scroll reveal
+// srtop.reveal('.keyfeature .keyfeature-heading', { interval: 200 });
+// srtop.reveal('.keyfeature .keyfeature-content', { delay: 300 });
+
+// document.addEventListener('contextmenu', function (e) {
+//     e.preventDefault();
+// });
+// document.onkeydown = function (e) {
+//     if (e.keyCode === 123) {
+//         return false;
+//     }
+//     if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+//         return false;
+//     }
+//     if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+//         return false;
+//     }
+//     if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+//         return false;
+//     }
+//     if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+//         return false;
+//     }
+// };
